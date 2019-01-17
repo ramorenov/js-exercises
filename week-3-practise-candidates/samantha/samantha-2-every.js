26a477f805cd782c46a8f68a2c1de40e8f210974
Martyna Maron <mm@ctrl-group.com>
2019-01-17 22:02:40 +0000

    Add Group G Samantha's week 3 practice exercises

---
+++ week-3-practise-candidates/samantha/samantha-2-every.js
@@ -0,0 +1,27 @@
+//2
+
+/*
+  Following on from exercise 1...
+  The travellers have safely landed and are foraging for food in the natural wildlife.
+  There are bushes with many different colour berries.
+  The pink berries are the ONLY safe one's to eat.
+  If any other berries are present, it's best not to eat from the bush at all!
+  Using .every(), determine, if the bush has ALL PINK berries and is safe for the astronauts to eat from the bush.
+*/
+
+var berryColors = ["pink", "pink", "pink", "neon", "pink", "transparent"];
+
+var safeToEat; //Complete this statement
+
+if (safeToEat) {
+  console.log("This is safe to eat");
+} else {
+  console.log("Toxic! Leave this alone");
+}
+
+/*
+  EXPECTED OUTPUT
+
+  Toxic! Leave this alone"
+
+*/
---
+++ week-3-practise-candidates/samantha/samantha-3-includes-filter.js
@@ -0,0 +1,33 @@
+//3
+
+/*
+  Continuing on from Exercise 2...
+  The voyagers decide that they quite like this planet, and some of them want to settle there and colonise it.
+  They decide that the all the families (whose last names starts with 'A') should stay, while the others go on in search of home or other land.
+  Use .includes() find families.
+  Using .filter() make a new array of colonisers that will stay.
+*/
+
+var voyagers = [
+    "Adam family",
+    "Potter family",
+    "Eric",
+    "Button family",
+    "Jude",
+    "Carmichael",
+    "Bunny",
+    "Oscar family",
+    "Avery family",
+    "Archer family"
+  ];
+  
+  var colonisers; // Complete this statement
+  
+  console.log(colonisers);
+  
+  /* 
+    EXPECTED OUTPUT
+  
+    [ 'Adam family', 'Avery family', 'Archer family' ]
+  
+  */
