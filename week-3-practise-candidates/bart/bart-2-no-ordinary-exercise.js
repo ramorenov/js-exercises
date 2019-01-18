e32b1c3892fc827e9b257bd7f07faf32e48fdecd
nbogie <neillbogie@googlemail.com>
2019-01-18 23:15:51 +0000

    Update and rename bart-2-find.js to bart-2-no-ordinary-exercise.js

---
+++ week-3-practise-candidates/bart/bart-2-no-ordinary-exercise.js
@@ -0,0 +1,18 @@
+// Exercise 2
+
+/* 
+  You are given an array of names of musical artists.
+  Find the first artist whose name starts with S 
+  and is shorter than 5 letters (who is also, coincidentally, the best artist from the array!)
+*/
+
+var namesOfMusicalArtists = ["Prince", "Elvis", "Sade", "Beatles", "Madonna", "Adele"];
+
+// write your code here
+var shortNameThatStartsWithS;
+
+
+// Expected output = "Sade"
+//
+//BTW, Yes Sade is the best artist from that array.  It's a close call between her and Prince, 
+//but don’t start talking to me about Adele, please.
