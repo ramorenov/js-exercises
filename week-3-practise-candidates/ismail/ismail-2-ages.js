3635fcd15cfd33071700b60c5d94ff0cef75d06e
nbogie <neillbogie@googlemail.com>
2019-01-18 23:06:33 +0000

    Update ismail-2-ages.js

--- week-3-practise-candidates/ismail/ismail-2-ages.js
+++ week-3-practise-candidates/ismail/ismail-2-ages.js
@@ -1,10 +1,13 @@
-/* You are working a project in the council to give some benefits to the aged people older than 65. Find out them in a new array in the following list  */
+/* You are working on a project in the council to give some benefits to people older than 65. 
+Find the ages over 65, and store in the array: `agesOver65`.
 
-var numbers = [56, 45, 32, 67, 89, 41, 65, 62, 59, 73];
+*/
+
+var allAges = [56, 45, 32, 67, 89, 41, 65, 62, 59, 73];
  
-var findTheAgedOnes;
+var agesOver65;
 
-console.log(findTheAgedOnes);
+console.log(agesOver65);
 
 /* EXPECTED RESULT 
  -----------
