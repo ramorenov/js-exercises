0a720d42aed5644e14479350d1aa015d419fe112
nbogie <neillbogie@googlemail.com>
2019-01-18 22:50:38 +0000

    Update kadir-1-some.js

--- week-3-practise-candidates/kadir/kadir-1-some.js
+++ week-3-practise-candidates/kadir/kadir-1-some.js
@@ -1,5 +1,6 @@
 /*
-You are working in a car dealer. You have "carsOnSale" cars on sale at the moment at your database. You have learned that "Mercedes" has been sold in the morning and "newCarsForSale" cars added to the sale.
+You are working in a car dealer. You have "carsOnSale" cars on sale at the moment at your database. 
+You have learned that "Mercedes" has been sold in the morning and "newCarsForSale" cars added to the sale.
 - One Customer is calling you if you have "Toyota" car on sale.
 
 */
