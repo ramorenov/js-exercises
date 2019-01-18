62d07cc2d53cc68a61c2b349ecd51c00f4f9c88c
nbogie <neillbogie@googlemail.com>
2019-01-18 23:03:36 +0000

    Update ismail-1-join-words.js

--- week-3-practise-candidates/ismail/ismail-1-join-words.js
+++ week-3-practise-candidates/ismail/ismail-1-join-words.js
@@ -1,4 +1,5 @@
-/* In Turkish Language, some of the words are joined words and they are written together. Create a new function in order to get a new array with the joined words of each items in the array*/
+/* In Turkish Language, some of the words are joined words and they are written together. 
+Create a new function in order to get a new array with the joined words of each items in the array*/
 
 var newWords = [
   {firstword : "Ata", lastword: "turk"},
