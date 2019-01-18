ff8d7f3ff50a9a7dfd9d9cef7a63e7d003bc2cfb
nbogie <neillbogie@googlemail.com>
2019-01-18 23:24:05 +0000

    Update bart-3-some.js
    added more names to the input array.  clarified the exercise

--- week-3-practise-candidates/bart/bart-3-some.js
+++ week-3-practise-candidates/bart/bart-3-some.js
@@ -1,19 +1,26 @@
-// Exercise 3 (.some)
-
-
 // You have an array of names:
 
-var names = [ "Bart", "Samantha", "Abz", "Ivina", "Zareen"]
+var names = ['Abdullah', 'Ahmed', 'Ahmet', 'Alisina', 'Bart', 'Clement', 'Deepa', 'Elamin', 
+             'Ermiyas', 'Ismail', 'Ivina', 'Joan', 'Kadir', 'Madiha', 'Matthew', 'Miles', 
+             'Mohammad', 'Mohbeen', 'Mussie', 'Najla', 'Nina', 'Samantha', 'Sola', 'Tayo', 'Zareen']
 
-// You know that Abz is shortened from his full name "Abdullah", and Bart is shortened from “Bartholomew” (Yes my parents did that to me and high school was FULL of Simpsons and biblical reference jokes before you ask) ,you want to check if the the array has any shortened names.
+/* 
+You know that Abz is shortened from his full name "Abdullah", 
+and Bart is shortened from "Bartholomew" 
+(Yes, my parents did that to me and high school was FULL 
+of Simpsons and biblical reference jokes before you ask).
 
-// Write a function that checks for abbreviated names (4 characters or less)
+You want to check if the the array has any names that look like they might be shortened.
+*/
+
+// PART 1: Write a function that checks for names of 4 characters or less.
 
 // write code here
+var shortNames;
 
+// PART 2: Use another array method to check that ALL of the names
+// in shortNames are under 5 characters in length.
 
-
-
-// check your array of names using the .some () method.
+// write code here
 
 // Expected output = 'true'
