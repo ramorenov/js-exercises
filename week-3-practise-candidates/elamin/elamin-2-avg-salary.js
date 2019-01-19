d58b0c52048e56dee48e8f4c99e3292204c766c2
nbogie <neillbogie@googlemail.com>
2019-01-19 02:58:46 +0000

    Update and rename elamin-2-forEach.js to elamin-2-avg-salary.js

---
+++ week-3-practise-candidates/elamin/elamin-2-avg-salary.js
@@ -0,0 +1,22 @@
+/*
+You have an array named "employees" which contains all the employees of company, along with their salary.
+
+- Calculate and log the the average salary of the employees 
+
+- Hint use the method forEach.
+*/
+
+var employees = [
+  ["Ahmed", "£1500"],
+  ["Clement", "£1577.87"],
+  ["Elamin", "£1478.43"],
+  ["Adam", "£7684.5"],
+  ["Tayoa", "£3565.34"],
+  ["Nina", "£3565.12"]
+];
+
+console.log(average);
+
+/*Expected output
+3228.54
+*/
