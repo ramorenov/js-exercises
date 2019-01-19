8221075f064504434307ae357a1689603488209e
nbogie <neillbogie@googlemail.com>
2019-01-19 04:09:02 +0000

    Update and rename najla-3-some.js to najla-3-is-it-vegan.js

---
+++ week-3-practise-candidates/najla/najla-3-is-it-vegan.js
@@ -0,0 +1,21 @@
+/* 
+You need to check if a recipe contains only vegan ingredients.
+*/
+
+var notVegan = ["eggs", "butter", "milk", "cream", "meat", "fish"];
+
+var recipeIngredients = ["aubergine", "courgette", "butter", "rice", "eggs", "olives"];
+
+
+var isOnlyVegan; // complete this statement
+
+
+if(isOnlyVegan){
+  console.log("Only vegan ingredients found.");
+} else {
+  console.log("The ingredients are not all vegan.");
+}
+
+/* expected results 
+"The ingredients are not all vegan."
+*/
