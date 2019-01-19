11ed64ba22a9971c75a858bfc94d86fe6c13566b
Neill <neillbogie@googlemail.com>
2019-01-19 00:47:51 +0000

    improved wording for ivina-2-animals.js

---
+++ week-3-practise-candidates/ivina/ivina-2-animals.js
@@ -0,0 +1,37 @@
+/* 
+Everybody knows I love animals! So I have decided to check if all small animals have a short name.
+
+For each name, check if the name length is equal or less than 4, and make a message in a new array.
+
+If it's smaller or equal than 4 you should show the name and the message saying that x animal has a short name.
+
+If the name is longer make a message saying that is a long name, and the name of the animal.
+
+Look carefully at the expected result.
+*/
+
+var smallAnimalNames = [
+  "Fox",
+  "Cat",
+  "Dog",
+  "Flying squirrel",
+  "Hamster",
+  "Sloth",
+  "Otter",
+  "Guinea Pig"
+];
+
+var animalMessages; // complete this line
+console.log(animalMessages);
+
+/* EXPECTED RESULT
+  ----------------------------------------- 
+  ["Fox is a small animal with a small name.", 
+  "Cat is a small animal with a small name.",
+  "Dog is a small animal with a small name.", 
+  "Flying squirrel is a small animal but has a long name.", 
+  "Hamster is a small animal but has a long name.",
+  "Sloth is a small animal but has a long name.", 
+  "Otter is a small animal but has a long name.", 
+  "Guinea Pig is a small animal but has a long name."]
+   */
