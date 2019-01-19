1b9c6f83fe9f6b407390814a32b01ad4ce5e29ea
nbogie <neillbogie@googlemail.com>
2019-01-19 01:32:45 +0000

    Update and rename samantha-3-includes-filter.js to samantha-3-who-stays.js
    add some names which shouldn't be selected.
    clarified wording a little.

--- week-3-practise-candidates/samantha/samantha-3-who-stays.js
+++ week-3-practise-candidates/samantha/samantha-3-who-stays.js
@@ -1,21 +1,25 @@
-//3
-
 /*
   Continuing on from Exercise 2...
   The voyagers decide that they quite like this planet, and some of them want to settle there and colonise it.
-  They decide that the all the families (whose last names starts with 'A') should stay, while the others go on in search of home or other land.
-  Use .includes() find families.
-  Using .filter() make a new array of colonisers that will stay.
+  The call the planet "Alpha" and they decide that the FAMILIES whose last names start with 'A' should stay, 
+  while the others go on in search of other planets to call home.
+  
+  Use .filter() make a new array of colonisers that will stay, according to the above rules.
+  
+  NOTE: don't include any element that is not a "family".
+  Hint: Use .includes() on the strings, to find families.
 */
 
 var voyagers = [
     "Adam family",
     "Potter family",
     "Eric",
+    "Aldous", 
     "Button family",
     "Jude",
     "Carmichael",
     "Bunny",
+    "Asimov",
     "Oscar family",
     "Avery family",
     "Archer family"
