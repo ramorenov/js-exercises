7bb030193ea1afffd06c04db92d4069b918ba323
nbogie <neillbogie@googlemail.com>
2019-01-19 03:07:19 +0000

    Update elamin-2-avg-salary.js

--- week-3-practise-candidates/elamin/elamin-2-avg-salary.js
+++ week-3-practise-candidates/elamin/elamin-2-avg-salary.js
@@ -3,20 +3,22 @@ You have an array named "employees" which contains all the employees of company,
 
 - Calculate and log the the average salary of the employees 
 
-- Hint use the method forEach.
+- Hint: use the method forEach, or reduce
 */
 
 var employees = [
   ["Ahmed", "£1500"],
   ["Clement", "£1577.87"],
   ["Elamin", "£1478.43"],
-  ["Adam", "£7684.5"],
+  ["Adam", "£7684.50"],
   ["Tayoa", "£3565.34"],
   ["Nina", "£3565.12"]
 ];
 
 console.log(average);
 
-/*Expected output
+/*
+Expected output
+
 3228.54
 */
