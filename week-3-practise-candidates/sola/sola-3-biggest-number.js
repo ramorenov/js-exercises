4ac4ce80bc1ce1407b4fc0f738c724d8cdda3e65
nbogie <neillbogie@googlemail.com>
2019-01-19 00:57:55 +0000

    Update sola-3-biggest-number.js
    clearer

--- week-3-practise-candidates/sola/sola-3-biggest-number.js
+++ week-3-practise-candidates/sola/sola-3-biggest-number.js
@@ -1,10 +1,13 @@
-/*Biggest number possible: write a program that takes an array of single digit numbers and returns the biggest number possible, when all  digits are combined.  An array of numbers is provided.*/
+/*
+Biggest number possible: 
+
+Write a program that takes an array of single digit numbers and returns the biggest number possible by re-arranging digits.
+*/
 
 var  digits = [7, 2, 4, 3, 8]
 
 // input your code here
-
-
+var biggestNumber;
 
 
 comsole.log(biggestNumber);
