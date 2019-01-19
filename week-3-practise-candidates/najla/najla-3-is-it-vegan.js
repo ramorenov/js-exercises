8549f1d52580b1feb4c2ce447702a0932f272350
Neill <neillbogie@googlemail.com>
2019-01-19 04:25:20 +0000

    overhaul is-it-vegan

--- week-3-practise-candidates/najla/najla-3-is-it-vegan.js
+++ week-3-practise-candidates/najla/najla-3-is-it-vegan.js
@@ -1,21 +1,38 @@
 /* 
 You need to check if a recipe contains only vegan ingredients.
+
 */
 
-var notVegan = ["eggs", "butter", "milk", "cream", "meat", "fish"];
+var notVegan = ["eggs", "butter", "milk", "cream", "fish", "bacon", "sausages"];
 
-var recipeIngredients = ["aubergine", "courgette", "butter", "rice", "eggs", "olives"];
+var recipe1 = ["aubergine", "courgette", "butter", "rice", "eggs", "olives"];
 
+var recipe2 = ["lentils", "creamed coconut", "oil", "onion", "curry powder", "mushrooms"];
 
-var isOnlyVegan; // complete this statement
+var recipe3 = ["eggs", "bacon", "sausages", "beans"];
 
+var allRecipes = [recipe1, recipe2, recipe3];
 
-if(isOnlyVegan){
-  console.log("Only vegan ingredients found.");
-} else {
-  console.log("The ingredients are not all vegan.");
+function checkOneRecipe(ingredients){  
+  var isOnlyVegan; //TODO: Complete this line.
+
+  if(isOnlyVegan){
+    console.log("Only vegan ingredients found: " + ingredients);
+  } else {
+    console.log("The ingredients are NOT all vegan: "  + ingredients);
+  }  
 }
 
-/* expected results 
-"The ingredients are not all vegan."
+allRecipes.forEach(checkOneRecipe);
+
+/* Expected results: 
+
+The ingredients are NOT all vegan: aubergine,courgette,butter,rice,eggs,olives
+Only vegan ingredients found: lentils,creamed coconut,oil,onion,curry powder,mushrooms
+The ingredients are NOT all vegan: eggs,bacon,sausages,beans
+
+*/
+
+/* Hint: "hfr .fbzr() be .rirel(), nybat jvgu .vapyhqrf()" 
+(You can decode this at https://rot13.com/)
 */
