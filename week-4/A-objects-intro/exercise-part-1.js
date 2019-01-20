353f642b6eaa51ff61ea95b3f7a256b268fe7ad2
IsmailCtg <a.akbas.ismail@gmail.com>
2019-01-20 13:55:35 +0000

    Week4_A_1

--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -7,3 +7,32 @@ Assign each of them to a separate variable
 
 */
 
+var myCar = {
+    brand: "Mercedes"
+};
+
+console.log(myCar);
+
+var myMobile = {
+    screen: "9 inches"
+};
+
+console.log(myMobile);
+
+var myHat = {
+    colour: "gray"
+};
+
+console.log(myHat);
+
+var myHarddisk = {
+    memory: "1 TB"
+};
+
+console.log(myHarddisk);
+
+var myGlasses = {
+    number: 2
+};
+
+console.log(myGlasses);
