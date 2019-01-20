126a42f8186c0cd68497a90bcdc4eecab95c44c3
IsmailCtg <a.akbas.ismail@gmail.com>
2019-01-20 13:48:56 +0000

    Week4_A0

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,13 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+
+var myLaptop = {
+    brand: "Dell",
+    screen: 15,
+    colour: "black",
+    operator: "intel",
+    memory: "200 GB"
+};
+
+console.log(myLaptop);
