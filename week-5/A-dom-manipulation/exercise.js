2a48079e9271201345f7f8c4547a2f0a57e1963f
Nina <ninaadam117@gmail.com>
2019-01-31 18:26:17 +0000

    work5

--- week-5/A-dom-manipulation/exercise.js
+++ week-5/A-dom-manipulation/exercise.js
@@ -1,19 +1,28 @@
 /*
 Task 1
-=======
+=======alertBtn"
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
+var bgrChangeBtn = document.getElementById("bgrChangeBtn");
 
 
+bgrChangeBtn.addEventListener('click', changesMe)
+
+function changesMe() {
+    bgrChangeBtn.style.backgroundColor = "red";
+}
 
 /*
 Task 2
 ======
 
-When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
+When a user clicks the 'ALERT' button, an alert box should pop up with the
+ text "Thanks for visiting Bikes for Refugees!"
 */
-
+function (ALERT) {
+    alertBtn ()
+}
 
 
 /*
