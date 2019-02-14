0f07cd883a8139d93fac0e616f80142fa84fdc93
IsmailCtg <a.akbas.ismail@gmail.com>
2019-02-14 06:30:06 +0000

    Week_5_C

--- week-5/C-promises/exercise.js
+++ week-5/C-promises/exercise.js
@@ -1,101 +1,83 @@
-/*
-  Promises
-  ---------------------------------
-  Open index.html in your browser
-*/
-
-/*
-  EXERCISE 1
-  =======
-
-  Insert the result of promise1 into the #exercise1 element. Hint: .then()
-
-  EXPECTED RESULT: The #exercise1 element has textContent = "A Promising
-  Promise"
+Promise"
 */
 function exercise1() {
   var promise1 = resolvedPromise()
+  var promise1 = resolvedPromise().then(function(resolve) {
+    document.getElementById("exercise1").textContent = resolve;
+  });
 }
 
 /*
-  EXERCISE 2
-  =======
-
-  Insert the result of promise2 into the #exercise2 element. Hint: .catch()
-
-  EXPECTED RESULT: The #exercise2 element has textContent = "A Unpromising
+@@ -27,7 +29,9 @@ function exercise1() {
   Promise"
 */
 function exercise2() {
   var promise2 = rejectedPromise()
+  var promise2 = rejectedPromise().catch(function(reject) {
+    document.getElementById("exercise2").textContent = reject;
+  });
 }
 
 /*
-  EXERCISE 3
-  =======
-
-  Insert the result of promise3 into the #exercise3 element. Notice how it
-  takes longer than the others to finish
-
+@@ -40,7 +44,9 @@ function exercise2() {
   EXPECTED RESULT: The #exercise3 element has textContent = "A Longer Promise"
 */
 function exercise3() {
   var promise3 = delayedPromise()
+  var promise3 = delayedPromise().then(function(delay) {
+    document.getElementById("exercise3").textContent = delay;
+  });
 }
 
 /*
-  EXERCISE 4
-  =======
-
-  Take the result of promise4 and concatenate your name onto the end of it.
-  Then return your concatenated string as a promise. Finally insert the result
-  of your promise and insert it into the #exercise4 element
-
-  EXPECTED RESULT: The #exercise4 element has textContent = "A Promise from:
+@@ -55,7 +61,9 @@ function exercise3() {
   YOUR NAME"
 */
 function exercise4() {
   var promise4 = concatPromise()
+  var promise4 = concatPromise().then(function(concat) {
+    document.getElementById("exercise4").textContent = concat + "zareen";
+  });
 }
 
 /*
-  EXERCISE 5 (Stretch Goal)
-  =======
-  Start this exercise if you have completed all of the other exercises and
-  you understand them
-
-  Create a new Promise that resolves with the string "Hello Promises!".
-  Take the result of your promise and insert the it into the #exercise5
-  element
-
+@@ -70,9 +78,16 @@ function exercise4() {
   EXPECTED RESULT: The #exercise5 element has textContent = "Hello Promises!"
 */
 
 function exercise5() {
   // Write your implementation here
+  var Promise = newPromiseResolves();
+  Promise.then(result => {
+    document.getElementById("exercise5").textContent = result;
+  });
+}
+
+function newPromiseResolves() {
+  return Promise.resolve("Hello Promises!");
 }
 
 /*
-  EXERCISE 6 (Stretch Goal)
-  =======
-  Start this exercise if you have completed all of the other exercises and
-  you understand them
-
-  Create a new Promise that rejects with the string "Something went wrong!".
-  Take the result of your promise and insert the it into the #exercise6
-  element
-
-  EXPECTED RESULT: The #exercise6 element has textContent = "Something went
+@@ -89,41 +104,47 @@ function exercise5() {
   wrong!"
 */
 function exercise6() {
   // Write your implementation here
+  var Promise = newPromiseRejected();
+  Promise.catch(result => {
+    document.getElementById("exercise6").textContent = result;
+  });
 }
 
+function newPromiseRejected() {
+  return Promise.reject("Something went wrong!");
+}
 
 // 
+//
 // -------------------------------------
 // 
+//
 // DON'T EDIT THE FUNCTIONS BELOW!
 // THEY ARE PROVIDED FOR YOU
 //
@@ -103,22 +85,29 @@ function exercise6() {
 //
 function resolvedPromise() {
   return Promise.resolve('A Promising Promise')
+  return Promise.resolve("A Promising Promise");
 }
 
 function rejectedPromise() {
   return Promise.reject('A Unpromising Promise')
+  return Promise.reject("A Unpromising Promise");
 }
 
 function delayedPromise() {
   return new Promise((resolve) => {
+  return new Promise(resolve => {
     setTimeout(() => {
       resolve('A Longer Promise')
     }, 2000)
   })
+      resolve("A Longer Promise");
+    }, 2000);
+  });
 }
 
 function concatPromise() {
   return Promise.resolve('A Promise from: ')
+  return Promise.resolve("A Promise from: ");
 }
 
 exercise1()
@@ -127,3 +116,9 @@ exercise3()
 exercise4()
 exercise5()
 exercise6()
+exercise1();
+exercise2();
+exercise3();
+exercise4();
+exercise5();
+exercise6();
