c6fbccc13b0d805483bd6d837e4f13db3ca470a2
IsmailCtg <a.akbas.ismail@gmail.com>
2019-03-02 02:13:00 +0000

    refactoring

--- week-8-practise/1-code-refactoring.js
+++ week-8-practise/1-code-refactoring.js
@@ -1,10 +1,4 @@
-/* EXERCISE 1 - Code Refactoring
- * This script functions as a library. 
- * Its purpose is to provide a set of functions that users can use to manipulate and decorate text.
- *
- * Using the coding practices you have learnt (you can use the syllabus as reference),
- * edit the code so that it establishes good coding standards. 
- *
+*
  * There may be both syntax and logical errors present.
  */
 
@@ -18,6 +12,10 @@ exports function reverseAndCaptilise(text)
 
   return reversed
   }
+  
+function reverseAndCapitilize(text) {
+  return text.toUpperCase().split('').reverse().join('');
+}
 
 // Function that returns text with every other letter capitlised
 export function everyOtherLetterCapitilised(text) {
@@ -26,10 +24,25 @@ export function everyOtherLetterCapitilised(text) {
        text[i] = text[i].toUpperCase() 
     }    
   } 
+function everyOtherLetterCapitilised(text) {
+  //converting text to array and making each letter an element of array
+  text = text.split('');
+  // looping through each letter of the array
+  for (let i = 0; i < text.length; i++) {
+    if (i % 2 === 0) {
+      //converting letter to uppercase if the index number is even
+      text[i] = text[i].toUpperCase();
+    }
+  }
+  return text.join("");//converting array back to string
 }
 
 export  function lowerCase(text) {
   let text = text.toLowerCase()
 
   return text
+function lowerCase(text) {
+  return text.toLowerCase()
 }
+
+export { reverseAndCapitilize, everyOtherLetterCapitilised, lowerCase };
